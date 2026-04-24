5e6124cc6f688c338e383fef38a7122fb3b03916
cassienxumalo <Cassienxumalo@gmail.com>
2026-04-24 16:11:46 +0200

    added weather icon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#windSpeed");
+  let iconElement = document.querySelector("#icon")
+
+
+  iconElement.innerHTML = '<img src="${response.data.condition.icon_url}" class="weather-icom" />'
 
   cityElement.innerHTML = response.data.city;
 temperatureElement.innerHTML = temperature;
